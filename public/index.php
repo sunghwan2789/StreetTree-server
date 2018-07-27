3641240b8e0f56788ad86e31d9554ec25f144803
Bang Sunghwan <sunghwan2789@gmail.com>
2018-07-28 03:40:46 +0900

    dotenv 추가

--- public/index.php
+++ public/index.php
@@ -1,6 +1,9 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
+$dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
+$dotenv->load();
+
 $app = new App\Http\Kernel();
 
 $app->any('/', App\Http\Actions\HomeAction::class);
