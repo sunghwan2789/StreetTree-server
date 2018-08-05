07213530c217cf210ce3c02b1597a6dc8dea39aa
Bang Sunghwan <sunghwan2789@gmail.com>
2018-08-06 01:09:20 +0900

    주석 정리

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -6,26 +6,8 @@ use DI\ContainerBuilder;
 
 class Kernel extends App
 {
-    /**
-     * @inheritdoc
-     */
     protected function configureContainer(ContainerBuilder $builder)
     {
         $builder->addDefinitions(__DIR__ . '/../config.php');
-
-        // foreach ($this->getContainerDefinitions() as $definition) {
-        //     $builder->addDefinitions($definition());
-        // }
-    }
-
-    /**
-     * @return ContainerDefinitionInterface[]
-     */
-    protected function getContainerDefinitions()
-    {
-        return [
-            // new DoctrineDefinition(),
-            // new TwigDefinition(),
-        ];
     }
 }
